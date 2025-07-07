2e27cf0267df3162ccc4aeec93e38a53b7b7816c
defendimattia <defendi.mattia92@gmail.com>
2025-07-07 14:42:53 +0200

    Add singleProduct controller and page

--- src/main/java/org/lessons/springmvc/hello_spring_mvc/controller/MainController.java
+++ src/main/java/org/lessons/springmvc/hello_spring_mvc/controller/MainController.java
@@ -1,25 +1,33 @@
 package org.lessons.springmvc.hello_spring_mvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
-    
+
     @GetMapping("/")
-    public String home(){
+    public String home() {
         return "home";
     }
 
     @GetMapping("/about")
-    public String about(){
+    public String about() {
         return "about";
     }
 
     @GetMapping("/products")
-    public String allProducts(){
+    public String allProducts() {
         return "allProducts";
+    }
+
+    @GetMapping("/products/{productID}")
+    public String singleProducts(@PathVariable(name = "productID") String productID, Model model) {
+        model.addAttribute("productID", productID);
+        return "singleProduct";
     }
 }
