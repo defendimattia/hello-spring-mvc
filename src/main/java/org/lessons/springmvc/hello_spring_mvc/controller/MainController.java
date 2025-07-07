97a1409803087e191b0486715b3618d8e8c03465
defendimattia <defendi.mattia92@gmail.com>
2025-07-07 12:32:11 +0200

    Add Maincontroller controller and home

---
+++ src/main/java/org/lessons/springmvc/hello_spring_mvc/controller/MainController.java
@@ -0,0 +1,15 @@
+package org.lessons.springmvc.hello_spring_mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
